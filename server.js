df5bb57c3bf0751241c6af1fef393ac07fc82e05
KailasRJ <kailasrj11@gamil.com>
2025-03-16 14:42:08 +0530

    first commit to dev

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 // Simple Route
 app.get('/', (req, res) => {
-    res.send(`Hello World! 🚀 Kailas's demo server is running!`);
+    res.send(`Hello World! 🚀 Kailas's demo server is running on port ${PORT} !`);
 });
 
 // Health Check Route
@@ -15,4 +16,9 @@ app.get('/health', (req, res) => {
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
+});
+
+const customMessage = process.env.CUSTOM_MESSAGE || "Default message";
+app.get('/message', (req, res) => {
+    res.send(customMessage);
 });
